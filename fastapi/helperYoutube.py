a62e6ecc7e8f3be9889a609cd2f4e2f16be9e85b
Anuj Verma <42962743+Ashes47@users.noreply.github.com>
2024-01-02 20:36:25 +0530

    Update helperYoutube.py

--- fastapi/helperYoutube.py
+++ fastapi/helperYoutube.py
@@ -5,26 +5,30 @@ from models import TranscriptionResponse, Transcriptslot
 from typing import List
 
 def getVideoId(URL):
-  """
-  Examples:
-  - http://youtu.be/SA2iWivDJiE
-  - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
-  - http://www.youtube.com/embed/SA2iWivDJiE
-  - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
-  """
-  query = urlparse.urlparse(URL)
-  if query.hostname == 'youtu.be':
-    return query.path[1:]
-  if query.hostname in ('www.youtube.com', 'youtube.com'):
-    if query.path == '/watch':
-      p = urlparse.parse_qs(query.query)
-      return p['v'][0]
-    if query.path[:7] == '/embed/':
-      return query.path.split('/')[2]
-    if query.path[:3] == '/v/':
-      return query.path.split('/')[2]
-  # fail?
-  return None
+    """
+    Extract the YouTube video ID from various URL formats, including YouTube Shorts.
+
+    Examples:
+    - https://www.youtube.com/shorts/Fwy4_Q4lTZE
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse.urlparse(URL)
+    if query.hostname == "youtu.be":
+        return query.path[1:]
+    if query.hostname in ("www.youtube.com", "youtube.com"):
+        if query.path.startswith("/shorts/"):
+            return query.path.split("/")[2]
+        if query.path == "/watch":
+            p = urlparse.parse_qs(query.query)
+            return p["v"][0]
+        if query.path.startswith("/embed/"):
+            return query.path.split("/")[2]
+        if query.path.startswith("/v/"):
+            return query.path.split("/")[2]
+    return None
 
 
 def makeSlots(transcription) -> List[Transcriptslot]:
